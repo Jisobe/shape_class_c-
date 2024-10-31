9210a63a6494df6cd1d4fcb7d7121d29a0d512fa
Jen Isobe <91648032+Jisobe@users.noreply.github.com>
2024-10-31 16:55:18 -0500

    Add main functionality

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "Circle.h"
 #include "Rectangle.h"
 #include "Shape.h"
+#include <vector>
+#include <memory>
+using namespace std;
 
-void showShape( Shape& shape){
+void showShape(Shape& shape){
   shape.draw();
 }
 
@@ -15,6 +18,36 @@ int main(void){
   showShape(shape1);
   showShape(circle1);
   showShape(rectangle1);
+
+//Regular pointers
+  vector<Shape*> shapes;
+  Circle* circlePtr;
+  Rectangle* rectanglePtr;
+
+  circlePtr = new Circle;
+  shapes.push_back(circlePtr);
+  rectanglePtr = new Rectangle;
+  shapes.push_back(rectanglePtr);
+
+  for (unsigned int i = 0; i < shapes.size(); i++) {
+    shapes.at(i)->draw();
+  }
+
+  delete circlePtr;
+  delete rectanglePtr;
+
+//Smart pointers
+  vector<unique_ptr<Shape>> shapes2;
+  // unique_ptr<Circle> circlePtr2(new Circle);
+  auto circlePtr2 = make_unique<Circle>;
+  shapes2.emplace_back(circlePtr2);
+  // unique_ptr<Rectangle> rectanglePtr2(new Rectangle);
+  auto rectanglePtr2 = make_unique<Rectangle>;
+  shapes2.emplace_back(rectanglePtr2);
+
+  for (unsigned int i = 0; i < shapes2.size(); i++) {
+    shapes2.at(i)->draw();
+  }
 
   return 0;
 }
