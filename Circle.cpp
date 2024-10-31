52bfd92b9c601ac758ac6c4d3f7c3307aaf81d80
Jen Isobe <91648032+Jisobe@users.noreply.github.com>
2024-10-31 15:47:21 -0500

    Add setters and getters

--- Circle.cpp
+++ Circle.cpp
@@ -2,24 +2,20 @@
 #include <iostream>
 using namespace std;
 
-Circle::Circle() : radius(0.0) {
-  
-}
+Circle::Circle() : radius(0.0) {}
 
-Circle::Circle(float radius) : radius(radius) {
-
-}
+Circle::Circle(float radius) : radius(radius) {}
 
 void Circle::setRadius(float radius){
   this->radius = radius;
 }
 
 float Circle::getRadius() const{
-
+  return radius;
 }
 
 float Circle::getArea() const{
-
+  return 3.14 * radius * radius;
 }
 
 void Circle::draw() const{
